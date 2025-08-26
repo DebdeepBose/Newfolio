74f8900e165dfdb0cff7a14b5c48f6327fb8918c
Debdeep <dbworkofficial@gmail.com>
2025-08-27 01:29:04 +0530

    spline

--- src/Hero/HeroMain.jsx
+++ src/Hero/HeroMain.jsx
@@ -1,3 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
 export const HeroMain = () => {
-  return <div className="h-screen w-screen bg-black"></div>;
+  return (
+    <div className="h-screen w-screen bg-black">
+      <Spline scene="https://prod.spline.design/GHlOXEus1N0oQFxa/scene.splinecode" />
+    </div>
+  );
 };
