0f4c0d56a9001ba2855c01f0780f206390a99c03
Debdeep <dbworkofficial@gmail.com>
2025-08-27 04:56:59 +0530

    nav

--- src/Hero/HeroMain.jsx
+++ src/Hero/HeroMain.jsx
@@ -4,7 +4,7 @@ import { ProfileCard } from "./ProfileCard";
 export const HeroMain = () => {
   return (
     <div className="h-screen w-screen bg-black flex flex-col md:flex-row items-start justify-center pt-16">
-      <div className="w-full md:w-1/2 relative flex items-center justify-center">
+      <div className="w-full md:w-1/2 relative flex items-center justify-center mb-8 md:mv-0">
         <ProfileCard />
       </div>
 
