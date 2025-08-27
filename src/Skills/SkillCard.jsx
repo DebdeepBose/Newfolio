8f33049d781ed1939787b50b6fe2ef9cc2b8c3de
Debdeep <dbworkofficial@gmail.com>
2025-08-28 02:00:11 +0530

    red

--- src/Skills/SkillCard.jsx
+++ src/Skills/SkillCard.jsx
@@ -1,6 +1,6 @@
 export const SkillCard = ({ icon: Icon, title }) => {
   return (
-    <div className="group w-[190px] h-[120px] bg-black text-center rounded-2xl shadow-inner transition-all duration-500 hover:h-[180px] border-2 border-teal-400 hover:shadow-[0_0_25px_3px_rgba(45,212,191,0.7)]">
+    <div className="group w-[120px] md:w-[190px] h-[120px] bg-black text-center rounded-2xl shadow-inner transition-all duration-500 hover:h-[180px] border-2 border-teal-400 hover:shadow-[0_0_25px_3px_rgba(45,212,191,0.7)]">
 
       <div className="w-[80px] h-[80px] flex items-center justify-center text-teal-400 text-6xl bg-black mx-auto mt-5 rounded-md transition-all duration-500 group-hover:text-teal-300">
         <Icon />
