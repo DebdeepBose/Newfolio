b594f8ddbccb8c971a8bfc7f69ba236ad18637c6
Debdeep <dbworkofficial@gmail.com>
2025-08-28 02:06:43 +0530

    screen

--- src/About/AboutMain.jsx
+++ src/About/AboutMain.jsx
@@ -4,11 +4,11 @@ import { FaUser } from "react-icons/fa";
 
 export const AboutMain = () => {
   return (
-    <div id="about-section" className="mt-16  md:pl-36 h-full w-screen bg-black flex flex-col items-center justify-center pt-16 md:pr-8">
-      <h2 className="flex items-center justify-center text-4xl md:text-5xl font-semibold text-teal-300 mb-24 text-center md:mr-32 mr-2">
+    <div id="about-section" className="mt-20 h-full w-screen bg-black flex flex-col items-center justify-center pt-24 md:pl-32">
+      <h2 className="flex items-center justify-center text-4xl md:text-5xl font-semibold text-teal-300 mb-24 md:mr-28 text-center">
         <FaUser className="mr-2" /> About Me
       </h2>
-      <div className="flex flex-col-reverse md:flex-row items-start justify-center w-full">
+      <div className="flex flex-col-reverse md:flex-row items-center md:items-start justify-center w-full">
         <AboutLeft />
         <div className="w-full md:w-1/2 relative flex items-center justify-center mb-8 md:mb-0">
           <ProfileCard />
--- src/Hero/HeroMain.jsx
+++ src/Hero/HeroMain.jsx
@@ -3,8 +3,8 @@ import { ProfileCard } from "./ProfileCard";
 
 export const HeroMain = () => {
   return (
-    <div className="h-full w-screen bg-black flex flex-col md:flex-row items-start justify-center pt-20 md:pr-16 pr-4 ">
-      <div className="w-full md:w-1/2 relative flex items-center justify-center mb-8 md:mv-0">
+    <div className="h-full w-screen bg-black flex flex-col md:flex-row items-center md:items-start justify-center pt-20 md:pr-32">
+      <div className="w-full md:w-1/2 relative flex items-center justify-center mb-8 md:mb-0">
         <ProfileCard />
       </div>
 
