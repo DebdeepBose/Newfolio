98e1aaabb61269e1fd8bc530398411f21617033f
Debdeep <dbworkofficial@gmail.com>
2025-09-12 12:07:40 +0530

    Resume update

--- src/About/ResumeButton.jsx
+++ src/About/ResumeButton.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const ResumeButton = () => {
   return (
     <StyledWrapper>
-        <a href="Res_pf.pdf" download="DDB_Res.pdf">
+        <a href="RESUME__.pdf" download="DDB_Res.pdf">
       <button className="button hover:scale-105 active:scale-95 transition-all">
         <svg viewBox="0 0 384 512" className="svgIcon">
           <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
